ff109c9e1740e82075b66ec18b8975cbe32e212b
Nguyễn Thế Dương <120582229+Duongthe@users.noreply.github.com>
2026-02-03 20:44:27 +0700

    m

---
+++ Lab08_QLSV_SQL/FrmMonHoc.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab08_QLSV_SQL
+{
+    public partial class FrmMonHoc : Form
+    {
+        string connectionString =
+        "Data Source=TANQUYEN\\SQLEXPRESS;Initial Catalog=StudentDB;Integrated Security=True;Trust Server Certificate=True";
+        public FrmMonHoc()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmMonHoc_Load(object sender, EventArgs e)
+        {
+            using SqlConnection conn = new SqlConnection(connectionString);
+            string sql = "SELECT * FROM Mon";
+
+            SqlDataAdapter da = new SqlDataAdapter(sql, conn);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            dataGridView1.DataSource = dt;
+        }
+    }
+}
